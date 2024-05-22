f02e133db6c91531ffc453c1148804d08080cdd0
kri-bak <kriba@aarhus.dk>
2024-05-22 15:49:11 +0200

    Cleanup

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -25,27 +25,32 @@ def process(orchestrator_connection: OrchestratorConnection) -> None:
     """Do the primary process of the robot."""
     orchestrator_connection.log_trace("Running process.")
 
+    # Get access tokens for Nova and email
     nova_credentials = orchestrator_connection.get_credential(config.NOVA_API)
     nova_access = NovaAccess(nova_credentials.username, nova_credentials.password)
-
     graph_credentials = orchestrator_connection.get_credential(config.GRAPH_API)
     graph_access = graph_authentication.authorize_by_username_password(graph_credentials.username, **json.loads(graph_credentials.password))
 
-    # Check mail for things to process
+    # Check mailbox for emails to process
     emails = get_emails(graph_access)
 
-    # Parse each mail and prepare for lookup in KMD Nova
+    # Parse each mail and add data to KMD Nova
     for email in emails:
+        # Get data from email
         case_mail = _parse_mail_text(email.body)
         list_of_ids = _get_ids_from_mail(email, graph_access)
         for ident in list_of_ids:
             queue_element = orchestrator_connection.create_queue_element(config.QUEUE_NAME, reference=f"{ident}", data=f"{case_mail.case_title, case_mail.note_text}", created_by="Robot")
+            # Clean up data
             ident = ident.replace("-", "")
-            cases = _get_cases_from_id(nova_access, ident, case_mail.case_title)
+            # Find a case to add note to
+            cases = nova_cases.get_cases(nova_access, ident, case_title = case_mail.case_title)
             name = _get_name_from_cpr(cases, ident, nova_access)
             case = _find_or_create_matching_case(cases, case_mail, ident, name, nova_access)
             nova_notes.add_text_note(case.uuid, case_mail.note_title, case_mail.note_text, True, nova_access)
+            # Set status Done for this note
             orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.DONE)
+        # Delete email
         graph_mail.delete_email(email, graph_access, permanent = True)
 
 
@@ -69,7 +74,13 @@ def get_emails(graph_access: GraphAccess) -> list[Email]:
 
 def _parse_mail_text(mail_text: str) -> CaseMail:
     ''' From an email sent by the OS2 Forms form "Masseoprettelse i KMD Nova",
-    create a new CaseMail object
+    create a new CaseMail object.
+
+    Args:
+        mail_text: Text from OS2 Forms that can be read by the robot.
+
+    "Returns:
+        A CaseMail object, for easier access to variables.
     '''
     return CaseMail(
         _get_line_after("Sagsoverskrift", mail_text),
@@ -81,14 +92,28 @@ def _parse_mail_text(mail_text: str) -> CaseMail:
     )
 
 
-def _get_sensitivity_from_email(email_string: str) -> str:
-    '''Translates the string from OS2 forms to the expected KMD Nova format. This is used only when creating a CaseMail
+def _get_line_after(line: str, text: str) -> str:
+    ''' Get the line after the indicated string, based on the format on OS2 mails, using regex.
 
     Args:
-        email_string: A string, matching the format in OS2 Forms and the emails sent to the robot
+        line: The string to search for.
+        text: The text to search in.
 
     Returns:
-        A string, matching the format expected by KMD Nova in the sensitivity field
+        The string between linebreaks, after the string searched for.
+    '''
+    match = re.search("<b>" + line + r"<\/b><br>(.+?)<br>", text)
+    return match.group(1)
+
+
+def _get_sensitivity_from_email(email_string: str) -> str:
+    '''Translates the string from OS2 forms to the expected KMD Nova format. This is used only when creating a CaseMail.
+
+    Args:
+        email_string: A string, matching the format in OS2 Forms and the emails sent to the robot.
+
+    Returns:
+        A string, matching the format expected by KMD Nova in the sensitivity field.
     '''
     translation = {
         "Fortrolige oplysninger": "Fortrolige",
@@ -99,21 +124,16 @@ def _get_sensitivity_from_email(email_string: str) -> str:
     return translation[email_string]
 
 
-def _get_line_after(line: str, text: str) -> str:
-    match = re.search("<b>" + line + r"<\/b><br>(.+?)<br>", text)
-    return match.group(1)
-
-
 def _get_name_from_cpr(cases: list[NovaCase], cpr: str, nova_access: NovaAccess) -> str:
-    '''Find name from a matching case or lookup by address
+    '''Find name from a matching case or lookup by address.
 
     Args:
-        cases: A list of NovaCases to check for existing name
-        cpr: ÍD of the person we are looking for
-        nova_access: A token to access the KMD Nova API
+        cases: A list of NovaCases to check for existing name.
+        cpr: ÍD of the person we are looking for.
+        nova_access: A token to access the KMD Nova API.
 
     Returns:
-        A string of a name
+        The name of the person with the provided CPR.
     '''
     # Check provided cases for an existing match with a name:
     for case in cases:
@@ -176,19 +196,21 @@ def _create_case(ident: str, name: str, case_mail: CaseMail, nova_access: NovaAc
 
 
 def _get_ids_from_mail(email: Email, graph_access: GraphAccess) -> list[str]:
+    ''' Open up attachment attached to an email and read the data contained.
+
+    Args:
+        email: An email.
+        graph_access: The accesstoken required to read emails.
+
+    Returns:
+        A list of each line found in the attachments.
+    '''
     id_list = list[0]
     attachments = graph_mail.list_email_attachments(email, graph_access)
     for attachment in attachments:
         email_attachment = graph_mail.get_attachment_data(attachment, graph_access)
-        id_list = email_attachment.read().decode().split()
+        id_list = id_list + email_attachment.read().decode().split()
     return id_list
-
-
-def _get_cases_from_id(nova_access: NovaAccess, id: str, case_title: str) -> list[NovaCase]:
-    if _is_cpr(id):
-        return nova_cases.get_cases(nova_access, id, case_title = case_title)
-    else:
-        return nova_cases.get_cvr_cases(nova_access, id, case_title = case_title)
 
 
 def _find_or_create_matching_case(cases: list[NovaCase], case_mail: CaseMail, ident: str, name: str, nova_access: NovaAccess) -> NovaCase:
@@ -199,28 +221,14 @@ def _find_or_create_matching_case(cases: list[NovaCase], case_mail: CaseMail, id
         case_mail: A CaseMail containing the data to check for
         ident, name: The identification and name to use if we need to create a new case
         nova_access: An access token to access the KMD Nova API
+
+    Returns:
+        A NovaCase which matches the arguments provided.
     '''
     for case in cases:
         if case.kle_number == case_mail.kle and case.title == case_mail.case_title:
             return case
     return _create_case(ident, name, case_mail, nova_access)
-
-
-def _is_cpr(string_to_check: str) -> bool:
-    ''' An adequate check on string to see if this is a CPR
-    Returns:
-        Is it 10 symbols long, without a hyphen?
-    '''
-    pure_number_string = string_to_check.replace("-", "")
-    return pure_number_string.__len__() == 10
-
-
-def _is_cvr(string_to_check: str) -> bool:
-    ''' An adequate test on a string to see if this is a CVR
-    Returns:
-        Is it 8 symbols long?
-    '''
-    return string_to_check.__len__() == 8
 
 
 if __name__ == '__main__':
